05fb8eaa0acfca45995851fcf4e30862bb0f5a07
David-Neto <davidrmachado@gmail.com>
2022-05-24 21:35:14 -0300

    Entrega do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  // Para nenhuma entrada:
+  if (animal === undefined) {
+    const animalCount = {};
+    data.species.forEach((animalNumbers) => {
+      animalCount[animalNumbers.name] = animalNumbers.residents.length;
+    });
+    return animalCount;
+  }
+  // Para somente espécie como entrada:
+  if (animal.specie !== undefined && animal.sex === undefined) {
+    return data.species.find(({ name }) => name === animal.specie).residents.length;
+  }
+  // Para espécie e sexo como entrada:
+  if (animal.sex !== undefined) {
+    const animais = data.species.find((animal1) => animal1.name === animal.specie);
+    return animais.residents.filter((animal2) => animal2.sex === animal.sex).length;
+  }
 }
 
 module.exports = countAnimals;
