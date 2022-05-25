5d77adbdd1363539ded80851804959a93a67ff2c
David-Neto <davidrmachado@gmail.com>
2022-05-25 03:41:36 -0300

    Entrega dos requisitos 6 e 7

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,10 +1,12 @@
 const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
 
+// Para verificar se a pessoa colaboradora é gerente:
 function isManager(id) {
   return employees.some((employee) => employee.managers.includes(id));
 }
 
+// Para retornar as pessoas lideradas pela gerência:
 function getRelatedEmployees(managerId) {
   if (isManager(managerId) !== true) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,51 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('6 - Cria uma função que retorna informações referentes aos elefantes', () => {
+  it('ao passar o argumento "count" retorna a quantidade de elefantes', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toEqual(expected);
+  });
+
+  it('ao passar o argumento "names" retorna um array com a relação dos nomes dos elefantes', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toEqual(expected);
+  });
+
+  it('ao passar o argumento "averageAge" retorna a média de idade dos elefantes', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toBeCloseTo(expected);
+  });
+
+  it('ao passar o argumento "location" retorna a localização dos elefantes', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toEqual(expected);
+  });
+
+  it('ao passar o argumento "popularity" retorna o valor da popularidade dos elefantes', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toEqual(expected);
+  });
+
+  it('ao passar o argumento "availability" retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    const actual = handlerElephants('availability');
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(actual).toEqual(expected);
+  });
+
+  it('ao não passar argumento retorna undefined', () => {
+    const actual = handlerElephants();
+    const expected = undefined;
+    expect(actual).toBe(expected);
+  });
+
+  it('ao passar um argumento que não é uma string retorna "Parâmetro inválido, é necessário uma string"', () => {
+    const actual = handlerElephants(100);
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    expect(actual).toBe(expected);
+  });
+});
