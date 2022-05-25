e283541971cdfd5f8e9db66d510763f5ac838665
David-Neto <davidrmachado@gmail.com>
2022-05-25 20:26:17 -0300

    Entrega do requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((visitor) => visitor.age < 18).length;
+  const adult = entrants.filter((visitor) => visitor.age >= 18 && visitor.age < 50).length;
+  const senior = entrants.filter((visitor) => visitor.age >= 50).length;
+  const visitors = { child, adult, senior };
+  return visitors;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) return 0;
+  if (entrants.length === undefined) return 0;
+  const group = countEntrants(entrants);
+  const entryValue = (group.child * data.prices.child) + (group.adult * data.prices.adult)
+  + (group.senior * data.prices.senior);
+  return entryValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
