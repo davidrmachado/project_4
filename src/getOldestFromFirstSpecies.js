6fcc58fdf54e920aacdbf2eea9b68c92b42b25d2
David-Neto <davidrmachado@gmail.com>
2022-05-27 19:54:30 -0300

    Entrega do requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstAnimal = data.employees.find((specie) => id === specie.id)
+    .responsibleFor.filter((idd) => data.species.find((animal) => animal.id === idd).name)[0];
+
+  const olderAnimal = data.species.find((specie) => specie.id === firstAnimal)
+    .residents.sort((first, second) => second.age - first.age)[0];
+
+  return Object.values(olderAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,46 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const allSpecies = species.map((specie) => specie.name);
+
+const daysOfweek = Object.keys(hours);
+const {week} = daysOfweek;
+console.log(daysOfweek);
+const itsMonday = {
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+
+// Função para gerar o objeto contendo o cronograma completo da semana:
+const completeSchedule = () => {
+
+}
+
+// Função para gerar o objeto contendo o cronograma de um dia:
+const scheduleOfTheDay = (day) => {
+  const object = {
+    day,
+  };
+  object.[`${daysOfweek}`]
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  //  Condição em que algum animal válido é passado como argumento:
+  if (allSpecies.includes(scheduleTarget)) {
+    return (species.find((specie) => specie.name === scheduleTarget)).availability;
+  }
+
+  // Condição em que recebe somente algum dia da semana como argumento:
+  if (daysOfweek.includes(scheduleTarget)) {
+    return scheduleOfTheDay(scheduleTarget);
+  }
+
+  // Condição em que recebe 'Monday' como argumento:
+  if (scheduleTarget === 'Monday') {
+    return itsMonday;
+  }
+
+  // Demais condições:
+  return completeSchedule(scheduleTarget);
 }
 
 module.exports = getSchedule;
